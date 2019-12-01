d8b292088a1df44d7175d592882f8d2ba4490eb0
Huli1984 <gabri.kitzune@gmail.com>
2019-12-01 23:24:37 +0100

    setting requirements - superuser

--- ADsPy/ADsPy/settings.py
+++ ADsPy/ADsPy/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # STATIC_ROOT = ''
-#STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/ADsPy/")
