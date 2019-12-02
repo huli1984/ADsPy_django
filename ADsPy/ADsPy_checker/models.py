48170c20bc18c95ba453808958e5f67467ea8561
Huli1984 <gabri.kitzune@gmail.com>
2019-12-02 16:26:05 +0100

    setting requirements - superuser - varchar

--- ADsPy/ADsPy_checker/models.py
+++ ADsPy/ADsPy_checker/models.py
@@ -52,7 +52,7 @@ class MySearch(models.Model):
     prof_two = BASE_DIR + "/ADsPy_checker/static/ADsPy/profile_two/"
     prof_three = BASE_DIR + "/ADsPy_checker/static/ADsPy/profile_three/"
     job_timeout = models.CharField(max_length=9, default=900)
-    insert_job_timeout = models.CharField(max_length=5, name="job timeout", default="00:15")
+    insert_job_timeout = models.CharField(max_length=25, name="job timeout", default="00:15")
 
     # Metadata
     class Meta:
